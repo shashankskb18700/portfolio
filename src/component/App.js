11afa72b294be2eecbd0f4e43e2c9c7265e50746
shashank <shashankskb18700@gmail.com>
2022-04-09 23:34:17 +0530

    inital commit

---
+++ src/component/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Home from "./Home/Home";
+const App = () => {
+  return (
+    <div>
+      <h2>e</h2>
+
+      <Home />
+    </div>
+  );
+};
+
+export default App;
